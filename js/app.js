69d3b48a3a46af71ba045b6a72f87204a11daa86
Michael Clasby <michaelclasby@hotmail.com>
2020-09-25 16:27:53 +0100

    Improved navbar mobile behaviour, changed colour scheme and improved project cards(still WIP)

--- js/app.js
+++ js/app.js
@@ -1,67 +1,66 @@
+const PDFStart = (nameRoute) => {
+  let loadingTask = pdfjsLib.getDocument(nameRoute),
+    pdfDoc = null,
+    canvas = document.querySelector("#cnv"),
+    ctx = canvas.getContext("2d"),
+    scale = 1.0,
+    numPage = 1;
 
-const PDFStart = nameRoute => {           
-    let loadingTask = pdfjsLib.getDocument(nameRoute),
-        pdfDoc = null,
-        canvas = document.querySelector('#cnv'),
-        ctx = canvas.getContext('2d'),
-        scale = 1.0,
-        numPage = 1;
+  const GeneratePDF = (numPage) => {
+    pdfDoc.getPage(numPage).then((page) => {
+      canvas.width = document.querySelector("#pdf").clientWidth;
 
-        const GeneratePDF = numPage => {
+      let unscaledview = page.getViewport({ scale: scale });
+      let currwidth = unscaledview.width;
 
-            pdfDoc.getPage(numPage).then(page => {
-                
-                canvas.width = document.querySelector('#pdf').clientWidth;
+      let viewport = page.getViewport({ scale: canvas.width / currwidth });
+      canvas.height = viewport.height;
 
-                let unscaledview = page.getViewport({ scale: scale });
-                let currwidth = unscaledview.width;
+      let renderContext = {
+        canvasContext: ctx,
+        viewport: viewport,
+      };
 
-                let viewport = page.getViewport({ scale: canvas.width/currwidth });
-                    canvas.height = viewport.height;
+      page.render(renderContext);
+    });
+  };
 
-            
-                let renderContext = {
-                    canvasContext : ctx,
-                    viewport:  viewport
-                }
-
-                page.render(renderContext);
-            })
-
-        }
-
-        
-        loadingTask.promise.then(pdfDoc_ => {
-            pdfDoc = pdfDoc_;
-            GeneratePDF(numPage)
-        });
-}
+  loadingTask.promise.then((pdfDoc_) => {
+    pdfDoc = pdfDoc_;
+    GeneratePDF(numPage);
+  });
+};
 
 const startPdf = () => {
-    PDFStart('../assets/CV.pdf')
-}
-
+  PDFStart("../assets/CV.pdf");
+};
 
 var resizeId;
-window.addEventListener('load', startPdf);
+window.addEventListener("load", startPdf);
 
-// window.addEventListener('resize', () => {
-    //     console.log("resized");
-    
-    //     clearTimeout(resizeId);
-    //     resizeId = setTimeout(startPdf, 500);
-    
-    // });
-    
-    
-    
-// Prevents canvas being drawn multiple times while resizing 
+// Prevents canvas being drawn multiple times while resizing
 var lastWidth = window.innerWidth;
-window.addEventListener('resize', () => {
-    if (window.innerWidth !== lastWidth) {
-        lastWidth = window.innerWidth;
-        clearTimeout(resizeId);
-        resizeId = setTimeout(startPdf, 300);
-    }
+window.addEventListener("resize", () => {
+  if (window.innerWidth !== lastWidth) {
+    lastWidth = window.innerWidth;
+    clearTimeout(resizeId);
+    resizeId = setTimeout(startPdf, 300);
+  }
 });
 
+var lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (window.innerWidth <= 400) {
+    const nav = document.getElementById("nav-container");
+
+    if ((nav.getBoundingClientRect().top = 10)) {
+      if (window.scrollY <= lastScrollY) {
+        nav.style.position = "sticky";
+      } else {
+        nav.style.position = "static";
+      }
+    }
+    lastScrollY = window.scrollY;
+  }
+});
